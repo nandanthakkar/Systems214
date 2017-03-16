eed5e0571328392f4f8af39d77ac57fb36b60d0f
11 <drudolph914@gmail.com>
2017-03-15 23:12:02 -0400

    started adding directories for each data structures

---
+++ projects/assignment2/runner.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	
+    //1. Go through file system
+	//2. Read file that is currently being read
+    //3. Populate hashtable
+    //4. Export data to XML
+
+	return 0;
+}
+
--- projects/assignment2/runner.h
+++ projects/assignment2/runner.h
@@ -1,7 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
 
+//booleans
 static enum _bool{false=0, true=1} bool;
 
+//HashNode, data type stored in the hashtable
 struct _HashNode{
 	char* filename;
 	int token_count;
@@ -11,8 +14,13 @@ struct _HashNode{
 //will resize as the hashtable resizes
 static HashNode tok_table[100];
 
+//linked list that stores the keys to the hashtable in alphanumeric order
+static HashNode keyset_root;
+
+//function to walk through the file system
 void stroll_through_files(char* root_dir);
 
+//function that reads the file
 void read_files(char* filepath);
 
 //goes through 'tok_table' and exports the data to xml
