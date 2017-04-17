d7b8eeeb6430169197ce676f342ea2f4292b5738
11 <drudolph914@gmail.com>
2017-04-16 20:19:46 -0400

    logging off for about an hour or so

--- real_assignment2/driver.c
+++ real_assignment2/driver.c
@@ -1,7 +1,15 @@
 #include "modules.h"
 #define ALPHA_SIZE 26;
 
+//TODO: Take for loop from print token_table function and add it here, 
+//TODO: Ensure to add data to xml file properly
+void writeToXML(){
 
+    
+
+}
+
+//TODO: Need to add function to recurse through filesystem
 
 //main method
 int main(){
