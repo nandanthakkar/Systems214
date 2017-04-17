bced91917def9b58106e3d4fe4e49da3fb3ef6fc
11 <drudolph914@gmail.com>
2017-04-17 00:34:46 -0400

    almost got the right output

--- real_assignment2/driver.c
+++ real_assignment2/driver.c
@@ -1,12 +1,33 @@
 #include "modules.h"
 #define ALPHA_SIZE 26;
 
-//TODO: Take for loop from print token_table function and add it here, 
 //TODO: Ensure to add data to xml file properly
 void writeToXML(){
-
     
+    printf("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
+    printf("<fileIndex>\n"); 
+    
+    //loop through alphabet
+    int i = -1;
+    for(i=0; i<26; i++){ 
+        //loop through tokens in hashtable
+        HashToken* itr = NULL;
+        for(itr = token_table[i]; itr!=NULL; itr=itr->next){
+            //loop through files in each token
+            
+            printf("\t<word text=\"%s\">\n", itr->token); 
+            
+            FileData* ptr = NULL;
+            int count=0;
+            
+            for(ptr=itr->head_fd; ptr!=NULL; ptr=ptr->next_fd, count++){
+                printf("\t\t<file name=\"%s\">%d</file>\n",ptr->filename, ptr->token_count);
+            }
+            printf("\t</word>\n");
+        }
+    }
 
+    printf("</fileIndex>");
 }
 
 //TODO: Need to add function to recurse through filesystem
@@ -28,7 +49,6 @@ int main(){
         addToken(token, filename);        
     }
     
-    printTokenTable();
-
+    writeToXML();
     return 0;
 }
