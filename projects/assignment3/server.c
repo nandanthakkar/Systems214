df71af7137eeaae20cf3be0885d5e5fc1bba1d10
11 <drudolph914@gmail.com>
2017-04-20 02:25:00 -0400

    creating a basic layout for the file and server

---
+++ projects/assignment3/server.c
@@ -0,0 +1,15 @@
+/*
+ * Author: Douglas Rudolph & Nandan Thakkar
+ * File: server.c
+ * Course: Systems Programming - CS:214
+ * Prorfessor: Dr. Fransisco
+ * Date Created: April 20th, 2017
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+    return 0;
+}
